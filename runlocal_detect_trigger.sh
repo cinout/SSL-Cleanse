08d0cf32207910bd01841c0c5c1055f224871cdb
seaskyho <seaskyho@gmail.com>
2024-09-12 15:48:09 +1000

    update

--- runlocal_detect_trigger.sh
+++ runlocal_detect_trigger.sh
@@ -2,5 +2,5 @@ cd detector
 
 python detector.py \
   --dataset imagenet --emb 128 --lr 1e-1 --bs 32 --epoch 1000 --lam 1e-1 --attack_succ_threshold 0.99 \
-  --fname ../checkpoint/imagenet/HTBA_BYOL_Imagenet100.tar --test_file_path ../data/imagenet/test_filelist.txt \
+  --fname ../checkpoint/imagenet/CTRL_SIMCLR_Imagenet100.tar --test_file_path ../data/imagenet/test_filelist.txt \
   --num_clusters 12 --knn_sample_num 1000 --ratio 0.05 --trigger_path ../trigger_estimation
